086a8e08c1f1860154d400e2722d4437be4e621f
Janel Raab <raab14@up.edu>
2014-03-03 23:54:33 -0800

    The menu scenes are made and the readme file is written

---
+++ Game/3DGame/Assets/MainMenu.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+
+	void OnGUI()
+	{
+		// Make the first button. If it is pressed, Application.Loadlevel (1) will be executed
+		if(GUI.Button(new Rect(100,540,120,50), "Start Game")) {
+			Application.LoadLevel("moveChanges");
+		}
+
+		// Make the second button.
+		if(GUI.Button(new Rect(260,540,120,50), "How to Play")) {
+			Application.LoadLevel("ControlInfo");
+		}
+
+		if(GUI.Button(new Rect(580,540,120,50), "Game Credits")) {
+			Application.LoadLevel("GameCredits");
+		}
+		
+		if(GUI.Button(new Rect(740,540,120,50), "Quit Game")) {
+			Application.Quit();
+		}
+		
+		
+	}
+	
+}
---
+++ Game/NimsHouse_README.txt
@@ -0,0 +1,12 @@
+Step 1)
+           Click on NimsHouse.exe
+Step 2)
+           A window will pop-up saying Library configuration.
+           Please make the screen size 1024x768
+           You can check the windowed box if you want
+                      This will just make the game in a window instead of full screen
+                      Either or will work just fine
+           Set the Grapics quality to anything you want
+Step 3)
+           Click Play!
+
